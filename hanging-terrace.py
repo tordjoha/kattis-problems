75188e223e2c3e189425fc392ed030e49fe4cf05
Tord Johansson <tord.johansson@student.uib.no>
2019-11-22 16:23:26 +0100

    Create hanging-terrace.py

---
+++ hanging-terrace.py
@@ -0,0 +1,16 @@
+tMax, e = map(int, raw_input().split(" "))
+curr = 0
+deniedGroups = 0
+for __ in range(e):
+	gAction, gCount = raw_input().split(" ")
+	
+	gCount = int(gCount)
+	if gAction == 'enter':		
+		if (curr + gCount) <= tMax:
+			curr += gCount
+		else:
+			curr += 0
+			deniedGroups += 1
+	else:
+		curr -= gCount
+print(deniedGroups)
