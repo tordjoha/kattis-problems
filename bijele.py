0c9b884ecdac3029de032a1553654caf73d2d4d4
Tord Johansson <tord.johansson@student.uib.no>
2019-11-25 11:40:38 +0100

    Create bijele.py

---
+++ bijele.py
@@ -0,0 +1,9 @@
+x = raw_input().split(" ")
+x[0] = 1 - int(x[0])
+x[1] = 1 - int(x[1])
+x[2] = 2 - int(x[2])
+x[3] = 2 - int(x[3])
+x[4] = 2 - int(x[4])
+x[5] = 8 - int(x[5])
+
+print(str(x[0]) + " " + str(x[1]) + " " + str(x[2]) + " " + str(x[3]) + " " + str(x[4]) + " " + str(x[5]))
