8c9af6cee5acc93851741c7a30eda04b7fac4698
Tord Johansson <tord.johansson@student.uib.no>
2019-12-01 16:56:49 +0100

    solved

---
+++ solving-for-carrots.py
@@ -0,0 +1,6 @@
+x = raw_input().split(" ")
+
+for __ in range(int(x[0])):
+	y = raw_input()
+
+print(int(x[1]))
---
+++ take-two-stones.py
@@ -0,0 +1,6 @@
+x = int(raw_input())
+
+if (x % 2) == 0:
+	print("Bob")
+else:
+	print("Alice")
