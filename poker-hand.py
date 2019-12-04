a9918338e4068b948b916094a3b2c2813272c24d
Tord Johansson <tord.johansson@student.uib.no>
2019-12-04 14:05:39 +0100

    change

--- poker-hand.py
+++ poker-hand.py
@@ -1,18 +1,11 @@
-from collections import Counter
+hand = map(str, input().split(" "))
+sList = []
+for card in hand:
+    x = card[0]
+    s = 1
+    for card2 in hand:
+        if x == card2[0]:
+            s += 1
+    sList.append(s)
 
-x = raw_input().split(" ")
-h1 = x[0]
-h2 = x[1]
-h3 = x[2]
-h4 = x[3]
-h5 = x[4]
-
-h1 = h1[-1:]
-h2 = h2[-1:]
-h3 = h3[-1:]
-h4 = h4[-1:]
-h5 = h5[-1:]
-
-array = [h1, h2, h3, h4, h5]
-handStrength = (Counter(array).keys())
-print(len(handStrength))
+print(max(sList))
