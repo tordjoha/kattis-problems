df0d9af7ec86bfc2b3bbf4d56ee09cfcfd74f4a5
Tord Johansson <tord.johansson@student.uib.no>
2019-12-04 14:13:41 +0100

    Update poker-hand.py

--- poker-hand.py
+++ poker-hand.py
@@ -1,11 +1,9 @@
-hand = map(str, input().split(" "))
+from collections import Counter
+
+hand = input().split(" ")
 sList = []
 for card in hand:
-    x = card[0]
-    s = 1
-    for card2 in hand:
-        if x == card2[0]:
-            s += 1
-    sList.append(s)
+    sList.append(card[:1])
+counts = (Counter(sList))
 
-print(max(sList))
+print(max(counts.values()))
