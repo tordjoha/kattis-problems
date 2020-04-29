87f795a69bd9dd2a4a11c3f49e2ee8084650dbd7
Tord Johansson <tordjoha1@gmail.com>
2020-04-29 21:54:00 +0200

    changed input to int lists

--- transit-woes.py
+++ transit-woes.py
@@ -1,10 +1,11 @@
-in1 = raw_input().split("")
-in2 = raw_input().split("")
-in3 = raw_input().split("")
-in4 = raw_input().split("")
+in1 = list(map(int, input().split()))
+in2 = list(map(int, input().split()))
+in3 = list(map(int, input().split()))
+in4 = list(map(int, input().split()))
 
 s = in1[0]
 t = in1[1]
 n = in1[2]
 
-
+theSum = sum(in2) + sum(in3) + sum(in4)
+print(theSum)
