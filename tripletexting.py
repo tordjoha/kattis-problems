3954ac64f967aeb9ccc1a7089dbd9d2ef4238d13
Tord Johansson <tordjoha1@gmail.com>
2020-06-01 16:42:13 +0200

    solved

--- tripletexting.py
+++ tripletexting.py
@@ -1,4 +1,16 @@
-userInput = list(input())
+userInput = input()
+wordLength = int(len(list(userInput))/3)
 
-for i in userInput:
-    
+firstWord = userInput[:wordLength]
+secondWord = userInput[wordLength:-wordLength]
+thirdWord = userInput[-wordLength:]
+finalWord = ""
+
+if( firstWord == secondWord):
+    finalWord = firstWord
+if (firstWord == thirdWord):
+    finalWord = firstWord
+if (secondWord == thirdWord):
+    finalWord = secondWord
+
+print(finalWord)
